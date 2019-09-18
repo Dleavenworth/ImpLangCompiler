ab8f222ff13dad2152ad4041754916374cf9edba
David Leavenworth <dleavenworth3@gmail.com>
2019-09-18 00:29:36 -0700

    Updated the regex to make it work with the keywords and new punctuation for Phase 1.2

--- token_definitions.py
+++ token_definitions.py
@@ -6,11 +6,13 @@ NUMBER = 'NUMBER'
 PUNCTUATION = 'PUNCTUATION'
 IGNORE = 'IGNORE'
 FAIL = 'FAIL'
+KEYWORD = 'KEYWORD'
 
 token_list = [
     (r'[\n\t ]+', IGNORE),
     # (r'#.*$', IGNORE), In case we want to handle comments later
-    (r'[\+\-\*\/\(\)]', PUNCTUATION),
+    (r'(?:\+|\-|\*|\/|\(|\)|(:=)|(;))', PUNCTUATION),
     (r'[0-9]+', NUMBER),
+    (r'(?:(if)|(then)|(else)|(endif)|(while)|(do)|(endwhile)|(skip))', KEYWORD),
     (r'([a-zA-Z])([a-zA-Z0-9])*', IDENTIFIER),
 ]
